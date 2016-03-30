4c552830811bbe212edb087acc3ef66a8623c0e9
BipinSutar <bpn.sutar@gmail.com>
2016-03-31 02:49:38 +0530

    first commit

---
+++ app/src/main/java/com/example/android/myappportfolio/app/DisplayMessageActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.myappportfolio.app;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
+    }
+}
